689b74ea7a5e471e1a9a7593eced7e7ac546bf14
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-09 22:28:00 +0200

    creata classe User

--- classes/User.php
+++ classes/User.php
@@ -1,6 +1,49 @@
 <?php
 
 class User{
+
+    protected $firstName;
+    protected $lastName;
+    protected $age;
+    protected $email;
+    protected $telephoneNumber;
+
+    public function __construct($_firstName, $_lastName, $_age, $_email, $_telephoneNumber){
+        $this->firstName = $_firstName;
+        $this->lastName = $_lastName;
+        $this->age = $_age;
+        $this->email = $_email;
+        $this->telephoneNumber = $_telephoneNumber;
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    public function getAge(){
+        return $this->age;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function getTelephoneNumber(){
+        return $this->telephoneNumber;
+    }
+
+    public function setTelephoneNumber($telephoneNumber){
+        $this->telephoneNumber = $telephoneNumber;
+    }
+
 }
 
 
