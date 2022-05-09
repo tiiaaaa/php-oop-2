c128d7179389a4e5a11c293857c41390c5d9f581
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-09 21:46:03 +0200

    aggiunte funzioni get alla classe product

--- classes/Product.php
+++ classes/Product.php
@@ -13,8 +13,9 @@ class Product{
     protected  $color;
     protected  $disponibility;
 
-    function __construct($_name, $_productId, $_description, $_price, $_vote, $_reviews, Size $_size, $_color, $_disponibility = false){
+    function __construct($_name, $_immage, $_productId, $_description, $_price, $_vote, $_reviews, Size $_size, $_color, $_disponibility = false){
         $this->name = $_name;
+        $this->immage = $_immage;
         $this->productId = $_productId;
         $this->description = $_description;
         $this->price = $_price;
@@ -23,6 +24,46 @@ class Product{
         $this->size = $_size;
         $this->color = $_color;
         $this->disponibility = $_disponibility;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getImmage(){
+        return $this->immage;
+    }
+
+    public function getProductId(){
+        return $this->productId;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function getVote(){
+        return $this->vote;
+    }
+
+    public function getReviews(){
+        return $this->reviews;
+    }
+
+    public function getSize(){
+        return $this->size;
+    }
+
+    public function getColor(){
+        return $this->color;
+    }
+
+    public function getDisponibility(){
+        return $this->disponibility;
     }
 }
 
